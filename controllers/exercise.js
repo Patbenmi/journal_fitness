df7f9d8511fcfee24e2bd4b9430ead7eab3a1c0c
Patrick Benjamin Nash <patbenmi@Patricks-MacBook-Pro.local>
2021-01-24 18:02:14 -0800

    Having issues accessing API

--- controllers/exercise.js
+++ controllers/exercise.js
@@ -2,11 +2,12 @@ let express = require('express')
 // const { in } = require('sequelize/types/lib/operators')
 let router = express.Router()
 let db = require('../models')
+const API_KEY = process.env.API_KEY
 const passport = require('../config/ppConfig.js')
 const LocalStrategy = require('passport-local')
 const { default: axios } = require('axios')
 const isLoggedIn = require('../middleware/isLoggedIn.js')
-const wgerUrl = "https://wger.de/api/v2/exercise/?language=2"
+const wgerUrl = "https://wger.de/api/v2/exercise/?language=2>"
 
 // router.get('/exercise', (req, res) =>{
 //   res.render('exercise.ejs')
@@ -17,6 +18,31 @@ router.get('/', isLoggedIn, (req, res) =>{
     console.log(apiResponse.data.results)
     res.render('exercise/exercise', { exercise: exercise.slice(0, 20)})
   })
+})
+
+router.get('/:id', isLoggedIn, (req, res) =>{
+  let currentUrl = `https://wger.de/en/exercise/${req.params.id}/view/.${API_KEY}`
+  axios.get(currentUrl).then(apiResponse => {
+    let exercise = apiResponse.data.results
+    console.log(apiResponse.data)
+    // console.log(apiResponse.data.results)
+    // res.render('exercise/search.ejs', {exercise: exercise})
+  }).catch(error => console.log(error))
+})
+
+router.post('/:id', isLoggedIn, (req, res) =>{
+  db.exercise.findOrCreate({
+    where: {
+      exerciseName: req.body.name
+    },
+    defaults:{
+      exerciseMuscle: req.body.muscles,
+      exerciseDescription: req.body.description,
+      exerciseLanguage: req.body.language
+    }
+  }).then(([exercise, wasCreated]) =>{
+    res.render('exercise/favorite', {exercise: exercise})
+  }).catch(error => console.log(error))
 })
 
 module.exports = router
